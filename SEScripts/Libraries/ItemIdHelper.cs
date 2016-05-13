2ce422b3d93b74ba5aec228a02749c26ac49f07f
mze9412 <github@mze9412.de>
2016-05-13 18:34:44 +0200

    Prepare new repository and project for SE scripts

---
+++ SEScripts/Libraries/ItemIdHelper.cs
@@ -0,0 +1,139 @@
+﻿
+using System.Collections.Generic;
+
+namespace mze9412.SEScripts.Libraries
+{
+    /**Begin copy here**/
+
+    public static class ItemIdHelper
+    {
+        private static List<ItemDefinition> ItemDefinitions;
+
+        static ItemIdHelper()
+        {
+            ItemDefinitions = new List<ItemDefinition>();
+
+            //add known ores
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ore", "Ice",            "Ice"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ore", "Iron",           "Fe"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ore", "Stone",          "Stone"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ore", "Cobalt",         "Co"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ore", "Gold",           "Au"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ore", "Magnesium",      "Mg"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ore", "Nickel",         "Ni"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ore", "Platinum",       "Pt"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ore", "Silicon",        "Si"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ore", "Silver",         "Ag"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ore", "Uranium",        "U"));
+
+            //add known ingots
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ingot", "Cobalt",       "Co"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ingot", "Gold",         "Au"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ingot", "Iron",         "Fe"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ingot", "Magnesium",    "Mg"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ingot", "Nickel",       "Ni"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ingot", "Platinum",     "Pt"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ingot", "Silicon",      "Si"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ingot", "Silver",       "Ag"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ingot", "Stone",        "Gravel"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Ingot", "Uranium",      "U"));
+
+            //add known components
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Component", "Computer",             "Computer"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Component", "Construction",         "Cons. Comp."));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Component", "Detector",             "Detector"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Component", "Girder",               "Girder"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Component", "GravityGenerator ",    "Grav. Gen."));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Component", "InteriorPlate ",       "Interior Pl. "));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Component", "LargeTube",            "Lg. Steel Tube"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Component", "Medical",              "Med. Comp."));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Component", "MetalGrid",            "Metal Grid"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Component", "Motor",                "Motor"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Component", "Reactor",              "Reactor Comp."));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Component", "SmallTube",            "Sm. Steel Tube"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_Component", "Thrust",               "Thruster Comp."));
+
+            //add known misc
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_GasContainerObject", "HydrogenBottle",  "H² Bottle"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_GasContainerObject", "OxygenBottle",    "O² Bottle"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_AmmoMagazine", "Missile200mm",          "Missile"));
+            ItemDefinitions.Add(new ItemDefinition("MyObjectBuilder_AmmoMagazine", "NATO_25x184mm",         "NATO_25x184mm"));
+        }
+
+        public static string GetDisplayName(string typeId, string subTypeId)
+        {
+            foreach (var item in ItemDefinitions)
+            {
+                if (item.TypeId == typeId && item.SubTypeId == subTypeId)
+                {
+                    return item.DisplayName;
+                }
+            }
+            return string.Empty;
+        }
+
+        public static bool IsOre(string typeId)
+        {
+            return typeId.EndsWith("_Ore");
+        }
+
+        public static bool IsIce(string typeId, string subTypeId)
+        {
+            return typeId.EndsWith("_Ore") && subTypeId == "Ice";
+        }
+
+        public static bool IsComponent(string typeId)
+        {
+            return typeId.EndsWith("_Component");
+        }
+
+        public static bool IsIngot(string typeId)
+        {
+            return typeId.EndsWith("_Ingot");
+        }
+
+        public static bool IsMisc(string typeId)
+        {
+            return !IsOre(typeId) && !IsIngot(typeId) && !IsComponent(typeId);
+        }
+
+        #region Item Definition class
+
+        /// <summary>
+        /// Container for known items
+        /// </summary>
+        private sealed class ItemDefinition
+        {
+            /// <summary>
+            /// Ctor
+            /// </summary>
+            /// <param name="typeId"></param>
+            /// <param name="subTypeId"></param>
+            /// <param name="displayName"></param>
+            public ItemDefinition(string typeId, string subTypeId, string displayName)
+            {
+                TypeId = typeId;
+                SubTypeId = subTypeId;
+                DisplayName = displayName;
+            }
+
+            /// <summary>
+            /// TypeId, something like MyObjectBuilder_Component
+            /// </summary>
+            public string TypeId { get; private set; }
+
+            /// <summary>
+            /// SubTypeId, usually internal name (i.e. Motor)
+            /// </summary>
+            public string SubTypeId { get; private set; }
+
+            /// <summary>
+            /// Display name for LCD printing
+            /// </summary>
+            public string DisplayName { get; private set; }
+        }
+
+        #endregion
+        /**End copy here**/
+    }
+}
---
+++ SEScripts/Libraries/LCDHelper.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mze9412.SEScripts.Libraries
+{
+    /**Begin copy here**/
+
+    /// <summary>
+    /// LCD Helper class
+    /// </summary>
+    public static class LCDHelper
+    {
+        static LCDHelper()
+        {
+            displays = new Dictionary<string, Display>();
+        }
+
+        /// <summary>
+        /// existing dictionaries
+        /// </summary>
+        private static readonly Dictionary<string, Display> displays;
+
+        private static bool AutoFlush = true;
+
+        /// <summary>
+        /// Spacer char
+        /// </summary>
+        public static char Spacer = '`';
+
+        #region Create / delete / print
+
+        /// <summary>
+        /// Creates a new display instance for the given LCD
+        /// </summary>
+        /// <param name="displayId"></param>
+        /// <param name="lcd"></param>
+        /// <returns></returns>
+        public static string CreateDisplay(string displayId, IMyTextPanel lcd)
+        {
+            displays.Add(displayId, new Display {Id = displayId, LCD = lcd, Builder = new StringBuilder()});
+            return displayId;
+        }
+
+        /// <summary>
+        /// Deletes display with given id
+        /// </summary>
+        /// <param name="displayId"></param>
+        public static void DeleteDisplay(string displayId)
+        {
+            if (displays.ContainsKey(displayId))
+            {
+                displays.Remove(displayId);
+            }
+        }
+
+        /// <summary>
+        /// Prints display to given terminal block, then deletes it
+        /// </summary>
+        /// <param name="displayId"></param>
+        /// <param name="append"></param>
+        public static void PrintDisplay(string displayId, bool append = false)
+        {
+            if (displays.ContainsKey(displayId))
+            {
+                var disp = displays[displayId];
+                disp.LCD.WritePublicText(disp.Builder.ToString(), append);
+                disp.LCD.ShowTextureOnScreen();
+                disp.LCD.ShowPublicTextOnScreen();
+            }
+        }
+
+        #endregion
+
+        #region Add lines
+
+        public static void WriteLine(string displayId, string line)
+        {
+            var disp = GetDisplay(displayId);
+            if (disp.HasValue)
+            {
+                disp.Value.Builder.AppendLine(line);
+            }
+
+            CheckAutoFlush(displayId);
+        }
+
+        public static void WriteHeader(string displayId, string line)
+        {
+            WriteLine(displayId, "__" + line + "__");
+        }
+
+        public static void WriteFormattedLine(string displayId, string format, params object[] args)
+        {
+            var disp = GetDisplay(displayId);
+            if (disp.HasValue)
+            {
+                disp.Value.Builder.AppendLine(string.Format(format, args));
+            }
+
+            CheckAutoFlush(displayId);
+        }
+
+        /// <summary>
+        /// Adds a progress bar with progress in %
+        /// </summary>
+        /// <param name="displayId"></param>
+        /// <param name="title"></param>
+        /// <param name="progress"></param>
+        public static void WriteProgressBar(string displayId, string title, double progress)
+        {
+            var disp = GetDisplay(displayId);
+            if (disp.HasValue)
+            {
+                var builder = disp.Value.Builder;
+                builder.Append(title.PadRight(15, Spacer));
+                builder.Append("[");
+
+                //we have 15 characters for progress bar
+                var usedChars = (int)Math.Round(30*progress, 0);
+                var restChars = 30 - usedChars;
+
+                for (int i = 0; i < usedChars; i++)
+                {
+                    builder.Append("I");
+                }
+                for (int i = 0; i < restChars; i++)
+                {
+                    builder.Append(Spacer);
+                }
+
+                builder.Append("] ");
+                builder.Append(string.Format("{0:0.00}%", progress*100));
+                builder.AppendLine();
+            }
+
+            CheckAutoFlush(displayId);
+        }
+
+        #endregion
+
+        #region Helper
+
+        private static void CheckAutoFlush(string displayId)
+        {
+            if (AutoFlush)
+            {
+                PrintDisplay(displayId, false);
+            }
+        }
+
+        private static Display? GetDisplay(string displayId)
+        {
+            if (displays.ContainsKey(displayId))
+            {
+                return displays[displayId];
+            }
+            return null;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// Struct to save display list
+        /// </summary>
+        private struct Display
+        {
+            public string Id { get; set; }
+
+            public IMyTextPanel LCD { get; set; }
+
+            public StringBuilder Builder { get; set; }
+        }
+        /**End copy here**/
+    }
+
+}
